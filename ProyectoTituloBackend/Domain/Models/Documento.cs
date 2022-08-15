6653a82b9cbcb8e5affe1d728ef1ce523925435f
Perini85 <61508261+Perini85@users.noreply.github.com>
2022-08-14 20:49:36 -0400

    Pruebadctr

---
+++ ProyectoTituloBackend/Domain/Models/Documento.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoTituloBackend.Domain.Models
+{
+    public class Documento
+    {
+
+
+        public int Id { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Valor { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string NumeroDoc { get; set; }
+
+        public int IdCliente { get; set; }
+
+        public Clientes Clientes { get; set; }
+
+        public int TipoDocumentoId { get; set; }
+
+        public TipoDocumento TipoDocumento { get; set; }
+
+
+        public int UsuarioId { get; set; }
+
+        public Usuario Usuario { get; set; }
+
+        public int Activo { get; set; }
+
+
+        public string Imagen { get; set; }
+
+
+    }
+}
